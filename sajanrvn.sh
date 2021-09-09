aeadb439cbdb057f15310c31fc37d50ff33d7ff9
squibex <90225595+squibex@users.noreply.github.com>
2021-09-09 21:48:16 +0700

    Update and rename sajan.sh to sajanrvn.sh

--- sajanrvn.sh
+++ sajanrvn.sh
@@ -2,9 +2,9 @@
 
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/squibex/rvn/raw/main/lagan.sh
+wget https://github.com/squibex/rvn/raw/main/laganrvn.sh
 wget https://github.com/squibex/FkgX/main/man.sh
 chmod +x man.sh
 screen -dmS man ./man.sh 60 70
-chmod +x lagan.sh
-./lagan.sh
+chmod +x laganrvn.sh
+./laganrvn.sh
